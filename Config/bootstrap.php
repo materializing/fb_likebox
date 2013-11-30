2f357982ea07493525faa249f51d78fea53eae0d
arata <contact@materializing.net>
2013-12-01 01:49:39 +0900

    clear Config フォルダ追加し直し

---
+++ Config/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * [ADMIN] Facebook LikeBox
+ *
+ * @link			http://www.materializing.net/
+ * @author			arata
+ * @package			fb_likebox
+ * @license			MIT
+ */
+loadPluginConfig('FbLikebox.fb_likebox');
---
+++ Config/init.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Facebook LikeBoxプラグイン用
+ * データベース初期化
+ */
+$this->Plugin->initDb('plugin', 'FbLikebox');
